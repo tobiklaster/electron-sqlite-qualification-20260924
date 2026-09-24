2b515f21a40faf6aecaaa328cfd3b49e2c261f5a
tobiklaster <tobiasklaster@icloud.com>
2026-09-25 00:37:55 +0200

    Repair install-script pinning for Vite lines without esbuild

--- scripts/pin-install-scripts.cjs
+++ scripts/pin-install-scripts.cjs
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const lock = JSON.parse(fs.readFileSync('package-lock.json', 'utf8'));
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const allowScripts = { 'better-sqlite3@13.0.3': true };
 const esbuild = lock.packages?.['node_modules/esbuild']?.version;
-if (!esbuild) throw new Error('resolved esbuild version missing');
-pkg.allowScripts = {
-  'better-sqlite3@13.0.3': true,
-  [`esbuild@${esbuild}`]: true
-};
+if (esbuild) allowScripts[`esbuild@${esbuild}`] = true;
+pkg.allowScripts = allowScripts;
 fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
-console.log(JSON.stringify({allowScripts: pkg.allowScripts}));
+console.log(JSON.stringify({allowScripts, esbuildPresent:Boolean(esbuild)}));
